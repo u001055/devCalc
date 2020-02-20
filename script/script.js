49c61cf1f5d0bb841438bf88e638a6ccab01485d
Oleg <52830599+u001055@users.noreply.github.com>
2020-02-21 01:18:27 +0300

    add calculation options

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const DATA = {
-    wichSite: ['landing', 'multiPage', 'onlineStore'],
+    whichSite: ['landing', 'multiPage', 'onlineStore'],
     price: [4000, 8000, 26000],
     desktopTemplates: [50, 40, 30],
     adapt: 20,
@@ -10,17 +10,22 @@ const DATA = {
     metrikaYandex: [500, 1000, 2000],
     analyticsGoogle: [850, 1350, 3000],
     sendOrder: 500,
-    deadlineDay: [[2, 7], [3, 10], [7, 14]],
+    deadlineDay: [
+        [2, 7],
+        [3, 10],
+        [7, 14]
+    ],
     deadlinePercent: [20, 17, 15]
 };
 
 const startButton = document.querySelector('.start-button'),
-firstScreen = document.querySelector('.first-screen'),
-mainForm = document.querySelector('.main-form'),
-formCalculate = document.querySelector('.form-calculate'),
-endButton = document.querySelector('.end-button'),
-total = document.querySelector('.total'),
-fastRange = document.querySelector('.fast-range');
+    firstScreen = document.querySelector('.first-screen'),
+    mainForm = document.querySelector('.main-form'),
+    formCalculate = document.querySelector('.form-calculate'),
+    endButton = document.querySelector('.end-button'),
+    total = document.querySelector('.total'),
+    fastRange = document.querySelector('.fast-range'),
+    totalPriceSum = document.querySelector('.total_price__sum');
 
 function showElem(elem) {
     elem.style.display = 'block';
@@ -30,20 +35,68 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
-function handlerCallBackForm() {
+function priceCalculation(elem) {
+    let result = 0,
+        index = 0,
+        options = [];
+
+    if (elem.name === 'whichSite') {
+        for (const item of formCalculate.elements) {
+            if (item.type === 'checkbox') {
+                item.checked = false;
+            }
+        }
+        hideElem(fastRange);
+    }
+
+
+    for (const item of formCalculate.elements) {
+        if (item.name === 'whichSite' && item.checked) {
+            index = DATA.whichSite.indexOf(item.value);
+
+        } else if (item.classList.contains('calc-handler') && item.checked) {
+            options.push(item.value);
+
+        }
+    }
+    options.forEach(function(key) {
+
+        if (typeof(DATA[key]) === 'number') {
+            if (key === 'sendOrder') {
+                result += DATA[key];
+            } else {
+                result += DATA.price[index] * DATA[key] / 100;
+            }
+        } else {
+            if (key === 'desktopTemplates') {
+                result += DATA.price[index] * DATA[key][index] / 100;
+            }
+        }
+    });
+
+    result += DATA.price[index];
+    totalPriceSum.textContent = result;
+}
+
+function handlerCallBackForm(event) {
     const target = event.target;
+
 
     if (target.classList.contains('want-faster')) {
         target.checked ? showElem(fastRange) : hideElem(fastRange);
     }
+
+    if (target.classList.contains('calc-handler')) {
+        priceCalculation(target);
+    }
 };
 
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
     showElem(mainForm);
     hideElem(firstScreen);
 });
 
-endButton.addEventListener('click', function() {
+endButton.addEventListener('click', function () {
     for (const elem of formCalculate.elements) {
         if (elem.tagName === 'FIELDSET') {
             hideElem(elem);
